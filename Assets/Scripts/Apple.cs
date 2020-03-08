98c809b6b7fc9cefe24964c23bd488c0eb47365a
Sabrina Wilson <sawilson3011@gmail.com>
2020-03-08 17:46:24 -0400

    Init merge of charControls

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public PlayerStats PlayerStatsObj = null;
+    //ScriptThatYouWant = GameObject.Find("PlayerStats").GetComponent<PlayerStats>();
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.tag == "Player") {
+            if(PlayerStatsObj != null) {
+                PlayerStatsObj.Heal((float) 1);
+            }
+            print("Item pickup");
+           // ScriptThatYouWant.Heal(1.0);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public Transform pfHealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+        Transform healthBarTransform = Instantiate(pfHealthBar, new Vector3(0, 10), Quaternion.identity);
+        HealthBar healthBar = healthBarTransform.GetComponent<HealthBar>();
+        healthBar.Setup(healthSystem);
+
+        //healthBar.Setup(healthSystem);
+        healthSystem.Damage(10);
+        Debug.Log("Health: " + healthSystem.GetHealthPercent());
+        healthSystem.Heal(100);
+        Debug.Log("Health: " + healthSystem.GetHealth());
+        healthSystem.Damage(90);
+    }
+
+}
