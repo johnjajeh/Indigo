cbc6840dedf4a29c4b77b57ab89de44019379078
Sabrina Wilson <sawilson3011@gmail.com>
2020-03-08 23:39:15 -0400

    Collectable compass, bounding walls

---
+++ Assets/Z3D/Compass/Scripts/CollectableCompass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableCompass : MonoBehaviour
+{
+     void OnTriggerEnter(Collider c) 
+    {
+        if(c.attachedRigidbody) 
+        {
+            // Check if object entering trigger can collect a compass
+            CompassCollector cc = c.attachedRigidbody.gameObject.GetComponent<CompassCollector>();
+            if (cc) 
+            {
+                // Destroy collected compass
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Z3D/Compass/Scripts/CompassCollector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassCollector : MonoBehaviour
+{
+    // Allows attached object to collect a compass
+    public bool hasCompass = false;
+
+    public void ReceiveRock() {
+        hasCompass = true;
+    }
+}
