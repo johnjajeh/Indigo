42d3bb35ed251168d5db5b5762e2af5240cf2b90
Mason Dominey <4masonimo2@gmail.com>
2020-03-08 20:10:48 -0400

    Finished n fixed win screen

---
+++ Assets/Scripts/WinMenuToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class WinMenuToggle : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if(canvasGroup.interactable)
+            {
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.alpha = 0f;
+
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.alpha = 1f;
+
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
