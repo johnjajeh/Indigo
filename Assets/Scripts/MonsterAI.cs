2f07aed9e71c2c78e20e526c38c4563bae34e01a
BradenChapman <bchapman34@gatech.edu>
2020-03-28 11:13:17 -0400

    Added AI patrolling game object and chasing player

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -14,29 +14,20 @@ public class MonsterAI : MonoBehaviour {
 	public AIState aiState;
 	public VelocityReporter velocity;
     public GameObject goMovingWP;
+    public double cutoffDistanceToMovingWaypoint = 2.0;
+
+    private int currWaypoint;
 
     public enum AIState {
 		frozen, // let it go
         chasingPlayer,
-		attackingPlayer
+		attackingPlayer,
+        patrolling
 	};
 
     public double timeElapsed;
 
-    private void setMovingWaypoint() {
-        // Debug.Log("Moving toward waypoint!");
-		float distance0 = (goMovingWP.transform.position - nM.transform.position).magnitude;
-    	float lookAheadT = distance0 / nM.speed;
-        Vector3 target = (goMovingWP.transform.position + (lookAheadT * velocity.Velocity));
-        // Debug.Log(target);
-    	nM.SetDestination(target);
-    }
-
-    private Vector3 getDistance() {
-    	float distance1 = (goMovingWP.transform.position - nM.transform.position).magnitude;
-    	float lookAheadT = distance1 / nM.speed;
-    	return (goMovingWP.transform.position + (lookAheadT * velocity.Velocity));
-    }
+ 
     
     // Start is called before the first frame update
     void Start()
@@ -45,10 +36,11 @@ public class MonsterAI : MonoBehaviour {
         nM.stoppingDistance = 1.0f;
         anim = GetComponent<Animator>();
         velocity = gameObject.AddComponent<VelocityReporter>();
-        aiState = AIState.chasingPlayer;
+        aiState = AIState.patrolling;
 
 
         timeElapsed = 0;
+        currWaypoint = 0;
     }
 
     // Update is called once per frame
@@ -61,12 +53,17 @@ public class MonsterAI : MonoBehaviour {
                 timeElapsed += Time.deltaTime;
 
                 if (timeElapsed >= 5) {
-                    aiState = AIState.chasingPlayer;
+                    if (amICloseEnoughToEthan()) {
+                        aiState = AIState.chasingPlayer;
+                    } else {
+                        aiState = AIState.patrolling;
+                        setNextWaypoint();
+                    }
                 }
                 break;        
             case AIState.chasingPlayer:
                 setMovingWaypoint();
-                if (!nM.pathPending && nM.remainingDistance - nM.stoppingDistance <= 3) {
+                if (!nM.pathPending && nM.remainingDistance - nM.stoppingDistance <= 2) {
                     // setNextWaypoint();   
                     aiState = AIState.attackingPlayer;
                     // anim.SetFloat("vely", nM.velocity.magnitude / nM.speed);
@@ -84,7 +81,44 @@ public class MonsterAI : MonoBehaviour {
                 aiState = AIState.frozen;
 
                 break;
-        }    
+            case AIState.patrolling:
+                Debug.Log("I am patrolling to waypoint " + currWaypoint);
+                if (!nM.pathPending && nM.remainingDistance - nM.stoppingDistance <= 0.2) {
+                    if (amICloseEnoughToEthan()) {
+                        Debug.Log("I am close enough to Ethan!");
+                        setMovingWaypoint();
+                        aiState = AIState.chasingPlayer;
+                    } else {
+                        setNextWaypoint();
+                    }
+                }
+                break; 
+        }
     
+    }
+
+    private bool amICloseEnoughToEthan() {
+        float distance = Mathf.Abs((nM.transform.position - goMovingWP.transform.position).magnitude);
+        return  distance < cutoffDistanceToMovingWaypoint;
+    }
+
+    private void setNextWaypoint() {
+        currWaypoint = (currWaypoint + 1) % waypoints.Length;
+        nM.SetDestination(waypoints[currWaypoint].transform.position);
+    }
+
+    private void setMovingWaypoint() {
+        // Debug.Log("Moving toward waypoint!");
+        float distance0 = (goMovingWP.transform.position - nM.transform.position).magnitude;
+        float lookAheadT = distance0 / nM.speed;
+        Vector3 target = (goMovingWP.transform.position + (lookAheadT * velocity.Velocity));
+        // Debug.Log(target);
+        nM.SetDestination(target);
+    }
+
+    private Vector3 getDistance() {
+    	float distance1 = (goMovingWP.transform.position - nM.transform.position).magnitude;
+    	float lookAheadT = distance1 / nM.speed;
+    	return (goMovingWP.transform.position + (lookAheadT * velocity.Velocity));
     }
 }
