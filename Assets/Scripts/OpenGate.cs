e4b6f2083d3cdc9572be02c50e55389f43c4b9d3
Mason Dominey <4masonimo2@gmail.com>
2020-04-06 00:03:54 -0400

    New walls with gate, key asset, animations, transition to house scene

---
+++ Assets/Scripts/OpenGate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenGate : MonoBehaviour
+{
+    public GameObject rightGate;
+    private Animator rightGateAnim;
+
+    public GameObject leftGate;
+    private Animator leftGateAnim;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider == null)
+        {
+            throw new System.ArgumentNullException(nameof(collider));
+        }
+
+        if (collider.CompareTag("Player") && Key.hasKey)
+        {
+            // Get animator component of both gates
+            rightGateAnim = rightGate.GetComponent<Animator>();
+            leftGateAnim = leftGate.GetComponent<Animator>();
+
+            // Set both gates animation states to Activated
+            rightGateAnim.SetBool("isActivated", true);
+            leftGateAnim.SetBool("isActivated", true);
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchToHouseScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchToHouseScene : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider == null)
+        {
+            throw new System.ArgumentNullException(nameof(collider));
+        }
+
+        if (collider.CompareTag("Player") && Key.hasKey)
+        {
+            Debug.Log("collision detected");
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
