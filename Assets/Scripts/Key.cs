f632b13119c253da6658e89d03c6a6725dfe56a0
Mason Dominey <4masonimo2@gmail.com>
2020-04-10 17:04:05 -0400

    Added house scene, new bounding walls, key, gate, animations

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public static bool hasKey;
+
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.tag == "Player") {
+            Debug.Log("Key pickup");
+            hasKey = true;
+
+            Destroy(gameObject);
+        }
+    }
+}
