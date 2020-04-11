12e5f4b7de1978d5b8ea6cf0e3eb1073297908cf
BradenChapman <bchapman34@gatech.edu>
2020-04-10 20:01:50 -0400

    Added enemy and friendly prefabs

---
+++ Assets/TextAppear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAppear : MonoBehaviour
+{
+    public GameObject billboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        billboard.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collider) {
+        billboard.SetActive(true);
+    }
+    public void OnTriggerExit(Collider collider) {
+        billboard.SetActive(false);
+    }
+}
