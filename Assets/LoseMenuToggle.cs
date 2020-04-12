608bf05a5024521b2894ffe459536db1c4e15642
Mason Dominey <4masonimo2@gmail.com>
2020-04-12 01:47:18 -0400

    Added lose & instruction screens

---
+++ Assets/LoseMenuToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseMenuToggle : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    //PlayerStats player = new PlayerStats();
+
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+    }
+
+    void Update()
+    {
+        if (PlayerStats.Instance.Health <= 0)
+        {
+            Debug.Log("lose menu toggle - lose entered");
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.alpha = 1f;
+
+            Time.timeScale = 0f;
+        }
+    }
+}
--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GameStarter : MonoBehaviour
 {
+    public string sceneName;
+
     // transition to main game scene
     public void StartGame()
     {
-        SceneManager.LoadScene("new3rdperson");
+        SceneManager.LoadScene(sceneName);
     }
 }
