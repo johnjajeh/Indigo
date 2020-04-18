add28fc1374de86c5a26e840104976481172c9fd
Jet171 <jonathannajjar10@gmail.com>
2020-04-18 19:17:05 -0400

    player animations and movement complete

---
+++ Assets/control/AJInput.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AJInput : MonoBehaviour
+{
+	AJMovement characterMove;
+
+	[System.Serializable]
+	public class InputSettings{
+		public string verticalAxis = "Vertical";
+		public string horizontalAxis = "Horizontal";
+		public string jumpButton = "Jump";
+	}
+	[SerializeField]
+	InputSettings input;
+
+	[System.Serializable]
+	public class OtherSettings {
+		public float lookSpeed = 5.0f;
+		public float lookDistance = 10.0f;
+		public bool requireInputForTurn = true;
+	}
+
+	[SerializeField]
+	public OtherSettings other;
+
+	Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterMove = GetComponent<AJMovement>();
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(characterMove) {
+       		characterMove.Animate(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+
+        	if(Input.GetButtonDown("Jump")) {
+        		characterMove.Jump();
+        	}
+        }
+
+        if(mainCam) {
+        	if(other.requireInputForTurn) {
+        		if(Input.GetAxis(input.horizontalAxis) != 0 || Input.GetAxis(input.verticalAxis) != 0) {
+        			CharacterLook();
+        		}
+        	}
+        	else {
+        		CharacterLook();
+        	}
+        }
+    }
+
+    //tells char to look at the point that is in front of camera
+    void CharacterLook() {
+    	Transform mainCamT = mainCam.transform;
+    	Transform pivotT = mainCamT.parent;
+    	Vector3 pivotPos = pivotT.position;
+    	//Vector3 mainCamPose = mainCamT.position;
+    	Vector3 lookTarget = pivotPos + (pivotT.forward * other.lookDistance);
+    	Vector3 thisPos = transform.position;
+    	Vector3 lookDir = lookTarget - thisPos;
+    	Quaternion lookRot = Quaternion.LookRotation(lookDir);
+    	lookRot.x = 0;
+    	lookRot.z = 0;
+    	Quaternion newRotation = Quaternion.Lerp(transform.rotation, lookRot, Time.deltaTime * other.lookSpeed);
+    	transform.rotation = newRotation;
+    }
+}
---
+++ Assets/control/scripts/AJMovement.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
+public class AJMovement : MonoBehaviour
+{
+
+	Animator animator;
+	CharacterController characterController;
+
+	[System.Serializable]
+	public class AnimationSettings{
+		public string verticalVelocityFloat = "Forward";
+		public string horizontalVelocityFloat = "Strafe";
+		public string groundedBool = "isGrounded";
+		public string jumpBool = "isJumping";
+
+
+	}
+
+	[SerializeField]
+	public AnimationSettings animations;
+
+	[System.Serializable]
+	public class PhysicsSettings {
+		public float gravityModifier = 9.81f;
+		public float baseGravity = 50.0f;
+		public float resetGravityValue = 1.2f;
+
+	}
+	[SerializeField]
+	public PhysicsSettings physics;
+
+	[System.Serializable]
+	public class MovementSettings {
+		public float jumpSpeed = 6;
+		public float jumpTime = 0.25f;
+
+	}
+	[SerializeField]
+	public MovementSettings movement;
+	bool jumping;
+	bool resetGravity;
+	float gravity;
+	bool isGrounded = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        SetupAnimator();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	ApplyGravity();
+    	isGrounded = characterController.isGrounded;
+        // Animate(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+
+        // if(Input.GetButtonDown("Jump")) {
+        // 	Jump();
+        // }
+    }
+
+    //animate the character also motion
+    public void Animate(float forward, float strafe) {
+    	animator.SetFloat(animations.verticalVelocityFloat, forward);
+    	animator.SetFloat(animations.horizontalVelocityFloat, strafe);
+    	animator.SetBool(animations.groundedBool, isGrounded);
+    	animator.SetBool(animations.jumpBool, jumping);
+    }
+
+    public void Jump() {
+    	if(jumping) {
+    		return;
+    	}
+    	if(isGrounded) {
+
+    		jumping = true;
+    		StartCoroutine(StopJump());
+    	}
+    }
+
+    IEnumerator StopJump() {
+    	yield return new WaitForSeconds(movement.jumpTime);
+    	jumping = false;
+    }
+
+    void ApplyGravity(){
+    	if(!characterController.isGrounded) {
+    		if(!resetGravity) {
+    			gravity = physics.resetGravityValue;
+    			resetGravity = true;
+    		}
+    		gravity += Time.deltaTime * physics.gravityModifier;
+    	} else {
+    		gravity = physics.baseGravity;
+    		resetGravity = false;
+    	}
+
+    	Vector3 gravityVector = new Vector3();
+
+    	if(!jumping) {
+    		gravityVector.y -= gravity; //maybe movement.gravity
+    	} else {
+			gravityVector.y = movement.jumpSpeed;
+    	}
+
+    	characterController.Move(gravityVector * Time.deltaTime);
+    }
+
+    //sets up animator for child avatar
+    private void SetupAnimator() {
+    	
+    	Animator[] animators = GetComponentsInChildren<Animator>();
+
+    	if(animators.Length > 0) {
+    		for(int i = 0; i < animators.Length; i++) {
+    			Animator anim = animators[i];
+    			Avatar av = anim.avatar;
+
+    			if(anim != animator) {
+    				animator.avatar = av;
+
+    				Destroy(anim);
+    			}
+    		}
+    	}
+    }
+
+
+}
