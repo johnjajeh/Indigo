bbed9e7cec91eb079e235a3b2b6dfea83afa48f5
BradenChapman <bchapman34@gatech.edu>
2020-04-20 12:06:23 -0400

    Added friendly NPC with hints that follows player. Press Q for hints

---
+++ Assets/HintAppear.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintAppear : MonoBehaviour
+{
+    public GameObject[] hints;
+    private int currentHint = 0;
+    private float timeSinceLastKeyPress = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < hints.Length; i++) {
+            hints[i].SetActive(false);
+        }
+        hints[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastKeyPress += Time.deltaTime;
+
+        if (timeSinceLastKeyPress >= 5) {
+            hints[currentHint].SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            hints[currentHint].SetActive(false);
+            currentHint = (currentHint+1) % hints.Length;
+            hints[currentHint].SetActive(true);
+            timeSinceLastKeyPress = 0.0f;
+        }
+    }
+}
---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent navMesh;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        navMesh.stoppingDistance = 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.SetDestination(player.transform.position);
+    }
+}
