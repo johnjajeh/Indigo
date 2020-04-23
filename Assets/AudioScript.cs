0fc2b058caf05d031c45bfe4f8bf25b26f744121
BradenChapman <bchapman34@gatech.edu>
2020-04-23 10:55:00 -0400

    Added sounds to compass, apple, and key pickups

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioSource keySound;
+    public AudioSource compassSound;
+    public AudioSource appleSound;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collision) {
+        GameObject g = collision.gameObject;
+        Debug.Log("In trigger! Tag is " + g.tag);
+        if (g.tag == "Compass") {
+            compassSound.Play();
+        } else if (g.tag == "Apple") {
+            appleSound.Play();
+        } else if (g.tag == "Key") {
+            keySound.Play();
+        }
+    }
+}
