0ae02b250dec4d1db85086e39f07a39cb41f997e
Jet171 <34073472+Jet171@users.noreply.github.com>
2020-04-28 18:36:34 -0400

    clean up assets

---
+++ Assets/PlayerFootsteps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    private AudioSource footsteps;					// AudioSource for footstep sound.
+
+    void Start()
+    {
+        footsteps = GetComponent<AudioSource>();
+        footsteps.volume = 0.1f;
+    }
+   
+    public void TakeStep()
+    {
+        footsteps.Play();
+    }
+}
