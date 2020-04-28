bf3791bcdd787a5fedcc4df78648fee887b04f41
Jet171 <34073472+Jet171@users.noreply.github.com>
2020-04-28 19:19:11 -0400

    input man

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,84 @@
+﻿/*
+ *  Author: ariel oliveira [o.arielg@gmail.com]
+ */
+
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private bool godMode = false;
+    public delegate void OnHealthChangedDelegate();
+    public OnHealthChangedDelegate onHealthChangedCallback;
+
+    #region Sigleton
+    private static PlayerStats instance;
+    public static PlayerStats Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<PlayerStats>();
+            return instance;
+        }
+    }
+    #endregion
+
+    [SerializeField]
+    private float health;
+    [SerializeField]
+    private float maxHealth;
+    [SerializeField]
+    private float maxTotalHealth = 0;
+
+    public float Health { get { return health; } }
+    public float MaxHealth { get { return maxHealth; } }
+    public float MaxTotalHealth { get { return maxTotalHealth; } }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("g"))
+        {
+            if(godMode) {
+                godMode = false;
+                Debug.Log("god mode is off");
+            } else {
+                godMode = true;
+                Debug.Log("god mode is on");
+            }
+        }
+    }
+
+    public void Heal(float health)
+    {
+        this.health += health;
+        ClampHealth();
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        if(!godMode){
+            health -= dmg;
+            ClampHealth();
+        }
+    }
+
+    public void AddHealth()
+    {
+        if (maxHealth < maxTotalHealth)
+        {
+            maxHealth += 1;
+            health = maxHealth;
+
+            if (onHealthChangedCallback != null)
+                onHealthChangedCallback.Invoke();
+        }   
+    }
+
+    void ClampHealth()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        if (onHealthChangedCallback != null)
+            onHealthChangedCallback.Invoke();
+    }
+}
